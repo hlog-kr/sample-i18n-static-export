86403529a15f29c626672648de21642e4ba47d53
hlog-kr <hlog.kr@gmail.com>
2023-09-18 23:05:56 +0900

    페이지에 다국어 적용

--- pages/second.js
+++ pages/second.js
@@ -1,7 +1,28 @@
+import {useTranslation} from "next-i18next";
+import {serverSideTranslations} from "next-i18next/serverSideTranslations";
+import Link from "next/link";
+
+// 이 부분이 빠지면 t('...') 동작 안함
+export async function getStaticProps({locale}) {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale, ['common'])),
+        },
+    };
+}
+
 export default function Second() {
+    const {t} = useTranslation(['common']);
+
     return (
         <div>
-            <h1>두 번째 화면</h1>
+            <h1>
+                {t('second')}
+            </h1>
+            <div>
+                <p><Link href={'/'}>{t('first')}</Link></p>
+                <p><Link href={'/second'} locale={'en'}>to English</Link> | <Link href={'/second'} locale={'ko'}>한국어로</Link></p>
+            </div>
         </div>
     )
 }
